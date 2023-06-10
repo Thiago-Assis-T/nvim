93105f62a38210f8cb181562ca0e7736bbb5eae3
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-06-09 22:19:04 -0300

    fixing longline error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,7 +13,8 @@ require'nvim-treesitter.configs'.setup {
   ignore_install = {  },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = '/some/path/to/store/parsers', -- Remember to run vim.opt.runtimepath:append('/some/path/to/store/parsers')!
+  -- parser_install_dir = '/some/path/to/store/parsers', 
+  -- Remember to run vim.opt.runtimepath:append('/some/path/to/store/parsers')!
 
   highlight = {
     enable = true,
