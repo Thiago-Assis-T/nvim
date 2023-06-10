39ac6717648b7810317b36b0231336e2d0acae15
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-06-09 22:03:38 -0300

    Replacing double quotes with single quotes and installing treesitter

--- lua/plugins/lazy-nvim.lua
+++ lua/plugins/lazy-nvim.lua
@@ -1,25 +1,28 @@
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
 		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
+require('lazy').setup({
 	{
-		"williamboman/mason.nvim",
-		build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+		'williamboman/mason.nvim',
+		build = ':MasonUpdate' -- :MasonUpdate updates registry contents
 	},
 	{
-		"folke/tokyonight.nvim",
+		'folke/tokyonight.nvim',
 		lazy = false,
 		priority = 1000,
 		opts = {},
+	},
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate'
 	}
 })
