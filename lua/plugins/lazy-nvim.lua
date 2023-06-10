6cfa8b17bf69cb58551a4761762fac385ef41111
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-06-10 15:09:14 -0300

    formatting and lintting

--- lua/plugins/lazy-nvim.lua
+++ lua/plugins/lazy-nvim.lua
@@ -1,34 +1,34 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
-	})
+   vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      lazypath,
+   })
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
-	{
-		'williamboman/mason.nvim',
-		build = ':MasonUpdate' -- :MasonUpdate updates registry contents
-	},
-	{
-		'williamboman/mason-lspconfig.nvim',
-	},
-	{
-		'neovim/nvim-lspconfig',
-	},
-	{
-		'folke/tokyonight.nvim',
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate'
-	}
+   {
+      'williamboman/mason.nvim',
+      build = ':MasonUpdate' -- :MasonUpdate updates registry contents
+   },
+   {
+      'williamboman/mason-lspconfig.nvim',
+   },
+   {
+      'neovim/nvim-lspconfig',
+   },
+   {
+      'folke/tokyonight.nvim',
+      lazy = false,
+      priority = 1000,
+      opts = {},
+   },
+   {
+      'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate'
+   }
 })
