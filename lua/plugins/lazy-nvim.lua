ff589f2589e899b7b2bfe6fe3cc2404da49e18b5
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-06-11 15:35:32 -0300

    added the lsp

--- lua/plugins/lazy-nvim.lua
+++ lua/plugins/lazy-nvim.lua
@@ -12,23 +12,43 @@ end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
    {
-      'williamboman/mason.nvim',
-      build = ':MasonUpdate' -- :MasonUpdate updates registry contents
-   },
-   {
-      'williamboman/mason-lspconfig.nvim',
-   },
-   {
-      'neovim/nvim-lspconfig',
-   },
-   {
-      'nvim-neo-tree/neo-tree.nvim',
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
       dependencies = {
-         'nvim-lua/plenary.nvim',
-         'nvim-tree/nvim-web-devicons',
-         'MunifTanjim/nui.nvim'
-      }
-   },
+         -- LSP Support
+         {'neovim/nvim-lspconfig'},             -- Required
+         {                                      -- Optional
+         'williamboman/mason.nvim',
+         build = function()
+            pcall(vim.cmd, 'MasonUpdate')
+         end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+   }
+},
+--  {
+--     'williamboman/mason.nvim',
+--     build = ':MasonUpdate' -- :MasonUpdate updates registry contents
+--  },
+--  {
+--     'williamboman/mason-lspconfig.nvim',
+--  },
+--  {
+--     'neovim/nvim-lspconfig',
+-- },
+{
+   'nvim-neo-tree/neo-tree.nvim',
+   dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim'
+   }
+},
    {
       'folke/tokyonight.nvim',
       lazy = false,
