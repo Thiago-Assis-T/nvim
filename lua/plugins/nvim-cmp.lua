329d58c950651cc066094ccd56c8510aba2f598f
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-06-14 10:25:01 -0300

    finished cmp stuff

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,47 +1,77 @@
 -- Set up nvim-cmp.
 local cmp = require("cmp")
 
+local kind_icons = {
+	Text = "",
+	Method = "m",
+	Function = "",
+	Constructor = "",
+	Field = "",
+	Variable = "",
+	Class = "",
+	Interface = "",
+	Module = "",
+	Property = "",
+	Unit = "",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "",
+	Event = "",
+	Operator = "",
+	TypeParameter = "",
+}
+
 cmp.setup({
-   snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-         require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-      end,
-   },
-   window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-   },
-   mapping = cmp.mapping.preset.insert({
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }),
-   }),
-   sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "nvim_lua" },
-      { name = "luasnip" },
-      { name = "path" },
-      { name = "buffer",  keyword_length = 5 },
-   }),
-   formatting = {
-      fields = { "kind", "abbr", "menu" },
-      format = function(entry, vim_item)
-         vim_item.menu = ({
-            nvim_lsp = "[LSP]",
-            luasnip = "[Snippet]",
-            buffer = "[Buffer]",
-            path = "[Path]",
-         })[entry.source.name]
-         return vim_item
-      end,
-   },
-   experimental = {
-      native_menu = false,
-      ghost_text = true,
-   },
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+		{ name = "path" },
+		{ name = "buffer", keyword_length = 5 },
+	}),
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			-- Kind icons
+			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				luasnip = "[Snippet]",
+				buffer = "[Buffer]",
+				path = "[Path]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
+	experimental = {
+		native_menu = false,
+		ghost_text = true,
+	},
 })
 -- Set up lspconfig.
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
