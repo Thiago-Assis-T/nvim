7fa57f852b139a15fb680bdc5a9993b7a0d6875b
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-06-17 11:25:43 -0300

    finally fixing the icons

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,46 +1,40 @@
 require("neo-tree").setup({
 	window = {
 		position = "left",
-		width = 30,
+		width = 25,
 		mapping_options = { noremap = true, nowait = true },
 	},
+
 	default_component_configs = {
 		filesystem = {
 			filtered_items = {
-				visible = true, -- when true, they will just be displayed differently than normal items
+				visible = false, -- when true, they will just be displayed differently than normal items
 				hide_dotfiles = false,
 				hide_gitignored = false,
 				hide_hidden = false, -- only works on Windows for hidden files/directories
 			},
 		},
-		icon = { folder_empty = "󰜌", folder_empty_open = "󰜌" },
-		git_status = { symbols = { renamed = "󰁕", unstaged = "󰄱" } },
+		icon = { folder_empty = "", folder_empty_open = "" },
+		git_status = { symbols = { renamed = "󰑕", unstaged = "" } },
 	},
 	document_symbols = {
 		kinds = {
 			File = { icon = "󰈙", hl = "Tag" },
-			Namespace = { icon = "󰌗", hl = "Include" },
-			Package = { icon = "󰏖", hl = "Label" },
-			Class = { icon = "󰌗", hl = "Include" },
-			Property = { icon = "󰆧", hl = "@property" },
-			Enum = { icon = "󰒻", hl = "@number" },
+			Namespace = { icon = "", hl = "Include" },
+			Package = { icon = "", hl = "Label" },
+			Class = { icon = "", hl = "Include" },
+			Property = { icon = "", hl = "@property" },
+			Enum = { icon = "", hl = "@number" },
 			Function = { icon = "󰊕", hl = "Function" },
-			String = { icon = "󰀬", hl = "String" },
+			String = { icon = "", hl = "String" },
 			Number = { icon = "󰎠", hl = "Number" },
-			Array = { icon = "󰅪", hl = "Type" },
+			Array = { icon = "", hl = "Type" },
 			Object = { icon = "󰅩", hl = "Type" },
-			Key = { icon = "󰌋", hl = "" },
-			Struct = { icon = "󰌗", hl = "Type" },
-			Operator = { icon = "󰆕", hl = "Operator" },
-			TypeParameter = { icon = "󰊄", hl = "Type" },
-			StaticMethod = { icon = "󰠄 ", hl = "Function" },
-		},
-	},
-	-- Add this section only if you've configured source selector.
-	source_selector = {
-		sources = {
-			{ source = "filesystem", display_name = " 󰉓 Files " },
-			{ source = "git_status", display_name = " 󰊢 Git " },
+			Key = { icon = "", hl = "" },
+			Struct = { icon = "", hl = "Type" },
+			Operator = { icon = "", hl = "Operator" },
+			TypeParameter = { icon = "", hl = "Type" },
+			StaticMethod = { icon = "", hl = "Function" },
 		},
 	},
 	-- Other options ...
