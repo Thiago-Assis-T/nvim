7c88811d5803904a76aa7fbbf81fb0d7481a4529
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-06-25 16:25:23 -0300

    fixing identation

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,14 +1,14 @@
 local function colorMyThing(color)
-    color = color or 'tokyonight-night'
-    vim.cmd.colorscheme(color)
+   color = color or 'tokyonight-night'
+   vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', ctermbg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none', ctermbg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none', ctermbg = 'none' })
-    vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'none', ctermbg = 'none' })
-    vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none', ctermbg = 'none' })
-    vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'none', ctermbg = 'none' })
-    vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'none', ctermbg = 'none' })
+   vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', ctermbg = 'none' })
+   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none', ctermbg = 'none' })
+   vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none', ctermbg = 'none' })
+   vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'none', ctermbg = 'none' })
+   vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none', ctermbg = 'none' })
+   vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'none', ctermbg = 'none' })
+   vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'none', ctermbg = 'none' })
 end
 
 colorMyThing()
--- lua/plugins/lsp/formatter.lua
+++ lua/plugins/lsp/formatter.lua
@@ -4,35 +4,35 @@ local api = vim.api
 
 -- Creating Format on Save functionality:
 api.nvim_create_autocmd('BufWritePre', {
-    callback = function()
-        vim.schedule(function()
-            cmd('FormatWrite')
-        end)
-    end,
+   callback = function()
+      vim.schedule(function()
+         cmd('FormatWrite')
+      end)
+   end,
 })
 
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require('formatter').setup({
-    -- Enable or disable logging
-    logging = false,
-    -- Set the log level
-    log_level = vim.log.levels.WARN,
-    -- All formatter configurations are opt-in
-    filetype = {
-        -- Formatter configurations for filetype "lua" go here
-        -- and will be executed in order
-        lua = {
-            -- "formatter.filetypes.lua" defines default configurations for the
-            -- "lua" filetype
-            require('formatter.filetypes.lua').stylua,
-        },
+   -- Enable or disable logging
+   logging = false,
+   -- Set the log level
+   log_level = vim.log.levels.WARN,
+   -- All formatter configurations are opt-in
+   filetype = {
+      -- Formatter configurations for filetype "lua" go here
+      -- and will be executed in order
+      lua = {
+         -- "formatter.filetypes.lua" defines default configurations for the
+         -- "lua" filetype
+         require('formatter.filetypes.lua').stylua,
+      },
 
-        -- Use the special "*" filetype for defining formatter configurations on
-        -- any filetype
-        ['*'] = {
-            -- "formatter.filetypes.any" defines default configurations for any
-            -- filetype
-            require('formatter.filetypes.any').remove_trailing_whitespace,
-        },
-    },
+      -- Use the special "*" filetype for defining formatter configurations on
+      -- any filetype
+      ['*'] = {
+         -- "formatter.filetypes.any" defines default configurations for any
+         -- filetype
+         require('formatter.filetypes.any').remove_trailing_whitespace,
+      },
+   },
 })
