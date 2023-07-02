6f28b5ff7cd95465d9cc82b653d538f16a5561be
Thiago-Assis-T <thiago.assisfernadnes@gmail.com>
2023-07-02 12:15:06 -0500

    removing stylelint

--- lua/plugins/lsp/linter.lua
+++ lua/plugins/lsp/linter.lua
@@ -1,9 +1,5 @@
 require('lint').linters_by_ft = {
    lua = { 'luacheck' },
-   css = { 'stylelint' },
-   scss = { 'stylelint' },
-   sass = { 'stylelint' },
-   less = { 'stylelint' },
 }
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
    callback = function()
