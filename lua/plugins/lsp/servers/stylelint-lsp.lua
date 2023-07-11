ef62e626a9d07305e87ea2f49c80e6ff0c0d8003
Thiago-Assis-T <thiago.assisfernadnes@gmail.com>
2023-07-11 17:15:12 -0300

    fixing stylelint lsp setup

--- lua/plugins/lsp/servers/stylelint-lsp.lua
+++ lua/plugins/lsp/servers/stylelint-lsp.lua
@@ -4,7 +4,14 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.stylelint_lsp.setup({
    capabilities = capabilities,
+   filetypes = { 'css' },
    settings = {
-      stylelintplus = {},
+      stylelintplus = {
+         enable = true,
+         autoFixOnFormat = true,
+         autoFixOnSave = true,
+         validateOnSave = true,
+         validateOnType = true,
+      },
    },
 })
