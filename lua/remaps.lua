6015cda356c6fcf83b2659efcd1aba71eb02e1c4
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-07-22 17:31:22 -0300

    telescope diagnostics

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -3,4 +3,5 @@ local opts = { silent = true, noremap = true }
 -- Telescope Remaps:
 vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>Telescope find_files<cr>', opts)
 vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>Telescope git_files<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>Telescope diagnostics<cr>', opts)
 vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>Telescope grep_string<cr>', opts)
