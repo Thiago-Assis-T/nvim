e7d96cd1309016cb91348166973e783cdef5ca9d
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-07-24 10:20:41 -0300

    ~

--- lua/plugins/lsp/servers/init.lua
+++ lua/plugins/lsp/servers/init.lua
@@ -1,7 +1,7 @@
 require('plugins.lsp.servers.lua-lsp')
 require('plugins.lsp.servers.emmet-lsp')
 require('plugins.lsp.servers.css-lsp')
--- require('plugins.lsp.servers.html-lsp')
+require('plugins.lsp.servers.html-lsp')
 require('plugins.lsp.servers.stylelint-lsp')
 require('plugins.lsp.servers.eslint-lsp')
 require('plugins.lsp.servers.tsserver-lsp')
