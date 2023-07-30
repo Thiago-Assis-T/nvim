99e0446117afcaa993b6d6bd6ea40d614a9e6fa4
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-07-30 14:12:41 -0300

    aaaaa

--- lua/plugins/lsp/servers/custom-elements-lsp.lua
+++ lua/plugins/lsp/servers/custom-elements-lsp.lua
@@ -1,1 +1,7 @@
-require('lspconfig').custom_elements_ls.setup({})
+local lspconfig = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.custom_elements_ls.setup({
+   capabilities = capabilities,
+})
