2677b238a0c9bfe457020bc582bcbbfe5e072abd
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-08-04 10:41:02 -0300

    deleted.

--- lua/plugins/lsp/servers/custom-elements-lsp.lua
+++
@@ -1,7 +0,0 @@
-local lspconfig = require('lspconfig')
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-lspconfig.custom_elements_ls.setup({
-   capabilities = capabilities,
-})
