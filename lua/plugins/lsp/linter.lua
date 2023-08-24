15ce4e51e83ad28b8d445da49e32c2686eb5e891
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-08-24 10:44:51 -0300

    stylelint

--- lua/plugins/lsp/linter.lua
+++ lua/plugins/lsp/linter.lua
@@ -6,6 +6,10 @@ require('lint').linters_by_ft = {
    typescriptreact = { 'eslint_d' },
    bash = { 'shellcheck' },
    yaml = { 'yamllint' },
+   css = { 'stylelint' },
+   scss = { 'stylelint' },
+   sass = { 'stylelint' },
+   less = { 'stylelint' },
 }
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
    callback = function()
