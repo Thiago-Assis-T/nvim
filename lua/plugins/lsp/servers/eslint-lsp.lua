2d67792157761d8b164c959e12a449088b03797a
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-09-27 10:50:40 -0300

    stuff

--- lua/plugins/lsp/servers/eslint-lsp.lua
+++ lua/plugins/lsp/servers/eslint-lsp.lua
@@ -3,5 +3,5 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.eslint.setup({
-   capabilities = capabilities,
+  capabilities = capabilities,
 })
